869ee89ec22e0a8d4c1fb830d03fb51b36cc6ad0
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2025-06-12 16:48:23 +0200

    use lambda expression

--- src/main/java/dev/mikablondo/repository/PersonRepository.java
+++ src/main/java/dev/mikablondo/repository/PersonRepository.java
@@ -22,47 +22,45 @@ public class PersonRepository {
         // Convert the Mutiny Pool to a classic Vert.x SQL Client Pool
         io.vertx.sqlclient.Pool classicClient = client.getDelegate();
 
-        return Multi.createFrom().emitter(emitter -> {
-            classicClient.getConnection().onComplete(connResult -> {
-                if (connResult.failed()) {
-                    emitter.fail(connResult.cause());
+        return Multi.createFrom().emitter(emitter -> classicClient.getConnection().onComplete(connResult -> {
+            if (connResult.failed()) {
+                emitter.fail(connResult.cause());
+                return;
+            }
+            SqlConnection conn = connResult.result();
+
+            // Prepare the SQL query to select all persons
+            conn.prepare("SELECT id, firstname, lastname FROM Person").onComplete(pqResult -> {
+                if (pqResult.failed()) {
+                    emitter.fail(pqResult.cause());
+                    conn.close();
                     return;
                 }
-                SqlConnection conn = connResult.result();
+                PreparedStatement preparedQuery = pqResult.result();
 
-                // Prepare the SQL query to select all persons
-                conn.prepare("SELECT id, firstname, lastname FROM Person").onComplete(pqResult -> {
-                    if (pqResult.failed()) {
-                        emitter.fail(pqResult.cause());
-                        conn.close();
-                        return;
-                    }
-                    PreparedStatement preparedQuery = pqResult.result();
+                // Create a stream to handle the results (batch size is set to 50)
+                RowStream<Row> stream = preparedQuery.createStream(50, Tuple.tuple());
 
-                    // Create a stream to handle the results (batch size is set to 50)
-                    RowStream<Row> stream = preparedQuery.createStream(50, Tuple.tuple());
+                stream.handler(row -> {
+                    Person person = Person.builder()
+                            .id(row.getLong("id"))
+                            .firstname(row.getString("firstname"))
+                            .lastname(row.getString("lastname"))
+                            .build();
 
-                    stream.handler(row -> {
-                        Person person = Person.builder()
-                                .id(row.getLong("id"))
-                                .firstname(row.getString("firstname"))
-                                .lastname(row.getString("lastname"))
-                                .build();
-
-                        // Emit the person object to the Multi stream
-                        emitter.emit(person);
-                    });
-                    stream.endHandler(v -> {
-                        emitter.complete();
-                        conn.close();
-                    });
-                    stream.exceptionHandler(err -> {
-                        emitter.fail(err);
-                        conn.close();
-                    });
+                    // Emit the person object to the Multi stream
+                    emitter.emit(person);
+                });
+                stream.endHandler(v -> {
+                    emitter.complete();
+                    conn.close();
+                });
+                stream.exceptionHandler(err -> {
+                    emitter.fail(err);
+                    conn.close();
                 });
             });
-        });
+        }));
     }
 }
 
